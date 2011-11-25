f54533e3d2530d01254bfb20f1878e215acf2f27
Quincy Tse <quincy.tse@sydney.edu.au>
2011-11-25 17:23:34 +1100

    Changed fetch functions to take a client instead of context.

--- src/fetch.go
+++ src/fetch.go
@@ -3,11 +3,17 @@ package movo
 import (
     "appengine"
     "appengine/urlfetch"
+    "http"
     "io"
     "os"
+    "strings"
     "url"
 
     "fmt"
+    )
+
+var (
+    C1AuthError = os.NewError("C1 Authentication Error")
     )
 
 func init() {
@@ -20,8 +26,7 @@ type C1ActDetail struct {
 }
 
 ///func GetNomRoll(c appengine.Context, actID uint) ([]C1Person, os.Error) {
-func GetNomRoll(c appengine.Context, actID uint) (string, os.Error) {
-  client := urlfetch.Client(c)
+func GetNomRoll(client *http.Client, actID uint) (string, os.Error) {
   resp, err := client.Get("https://www.cadetone.aafc.org.au/")
   if err != nil {
 ///    return nil, err
@@ -37,17 +42,16 @@ func GetNomRoll(c appengine.Context, actID uint) (string, os.Error) {
   return string(b), nil
 }
 
-func GetActDetail(c appengine.Context, actID uint) (C1ActDetail, os.Error)
+func GetActDetail(client *http.Client, actID uint) (C1ActDetail, os.Error)
 
 //TODO - use correct return type
-func GetPersDOB(c appengine.Context, serviceNo, session string) (int, os.Error)
+func GetPersDOB(client *http.Client, serviceNo, session string) (int, os.Error)
 
-func C1Login(c appengine.Context, usr, pwd string) (string, os.Error) {
+func C1Login(client *http.Client, usr, pwd string) (string, os.Error) {
   param := make(url.Values)
   param.Add("ServiceNo", usr)
   param.Add("Password", pwd)
 
-  client := urlfetch.Client(c)
   resp, err := client.PostForm("https://www.cadetone.aafc.org.au/logon.php", param)
   if err != nil {
     return "", err
@@ -59,6 +63,15 @@ func C1Login(c appengine.Context, usr, pwd string) (string, os.Error) {
   if err != io.ErrUnexpectedEOF {
     return fmt.Sprintf("%d",n), err
   }
+
+  if strings.Contains(string(b), "Please try again") {
+    return "", C1AuthError
+  }
+
   return string(b), nil
 ///  return fmt.Sprint(resp.Cookies()), nil
+}
+
+func GetClient(c appengine.Context) *http.Client {
+  return urlfetch.Client(c)
 }
