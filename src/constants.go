bd2f2f03c86325df5a137d4efcdc68c465159756
Quincy Tse <quincy.tse@sydney.edu.au>
2011-12-09 13:56:50 +1100

    Added AAC/ARA, ANC/RAN ranks

--- src/constants.go
+++ src/constants.go
@@ -29,32 +29,112 @@ func init() {
   RankOrder["WOFF(AAFC)"] = 16
   RankOrder["FSGT"] = 17
   RankOrder["FSGT(AAFC)"] = 17
-  RankOrder["SGT"] = 18
-  RankOrder["SGT(AAFC)"] = 18
-  RankOrder["CPL"] = 19
-  RankOrder["CPL(AAFC)"] = 19
-  RankOrder["LAC"] = 20
-  RankOrder["LAC(AAFC)"] = 20
-  RankOrder["LACW"] = 20
-  RankOrder["LACW(AAFC)"] = 20
-  RankOrder["AC"] = 21
-  RankOrder["AC(AAFC)"] = 21
-  RankOrder["ACW"] = 21
-  RankOrder["ACW(AAFC)"] = 21
-  RankOrder["NCOCDT"] = 22
-  RankOrder["ACR"] = 23
-  RankOrder["ACWR"] = 23
+  RankOrder["SGT"] = 19
+  RankOrder["SGT(AAFC)"] = 19
+  RankOrder["CPL"] = 20
+  RankOrder["CPL(AAFC)"] = 20
+  RankOrder["LAC"] = 22
+  RankOrder["LAC(AAFC)"] = 22
+  RankOrder["LACW"] = 22
+  RankOrder["LACW(AAFC)"] = 22
+  RankOrder["AC"] = 23
+  RankOrder["AC(AAFC)"] = 23
+  RankOrder["ACW"] = 23
+  RankOrder["ACW(AAFC)"] = 23
+  RankOrder["NCOCDT"] = 24
+  RankOrder["ACR"] = 25
+  RankOrder["ACWR"] = 25
 
-  RankOrder["CIV"] = 25
-  RankOrder["MR"] = 25
-  RankOrder["MRS"] = 25
-  RankOrder["MS"] = 25
+  RankOrder["CIV"] = 26
+  RankOrder["MR"] = 26
+  RankOrder["MRS"] = 26
+  RankOrder["MS"] = 26
 
   RankOrder["CUO"] = 32
   RankOrder["CWOFF"] = 33
   RankOrder["CFSGT"] = 34
-  RankOrder["CSGT"] = 35
-  RankOrder["CCPL"] = 36
-  RankOrder["LCDT"] = 37
-  RankOrder["CDT"] = 38
+  RankOrder["CSGT"] = 36
+  RankOrder["CCPL"] = 37
+  RankOrder["LCDT"] = 39
+  RankOrder["CDT"] = 40
+
+  RankOrder["GEN"] = 1
+  RankOrder["LTGEN"] = 2
+  RankOrder["MAJGEN"] = 3
+  RankOrder["RSM-A"] = 4
+  RankOrder["BRIG"] = 5
+
+  RankOrder["COL"] = 6
+  RankOrder["COL(AAC)"] = 6
+  RankOrder["LTCOL"] = 7
+  RankOrder["LTCOL(AAC)"] = 7
+  RankOrder["MAJ"] = 8
+  RankOrder["MAJ(AAC)"] = 8
+///  RankOrder["CAPT"] = 9
+  RankOrder["CAPT(AAC)"] = 9
+  RankOrder["LT"] = 10
+  RankOrder["LT(AAC)"] = 10
+  RankOrder["2LT"] = 11
+  RankOrder["2LT(AAC)"] = 11
+
+  RankOrder["WO1"] = 16
+  RankOrder["WO1(AAC)"] = 16
+  RankOrder["WO2"] = 17
+  RankOrder["WO2(AAC)"] = 16
+  RankOrder["SSGT"] = 18
+  RankOrder["SSGT(AAC)"] = 18
+///  RankOrder["SGT"] = 19
+  RankOrder["SGT(AAC)"] = 19
+///  RankOrder["CPL"] = 20
+  RankOrder["CPL(AAC)"] = 20
+  RankOrder["LCPL"] = 21
+  RankOrder["LCPL(AAC)"] = 21
+  RankOrder["PTE"] = 23
+  RankOrder["UA(AAC)"] = 23
+  RankOrder["OCDT"] = 24
+  RankOrder["RCT"] = 25
+
+  RankOrder["NATCUO"] = 32
+  RankOrder["RCUO"] = 32
+///  RankOrder["CUO"] = 32
+  RankOrder["NATCDTRSM"] = 33
+  RankOrder["CDTWO1"] = 33
+  RankOrder["CDTWO2"] = 34
+  RankOrder["CDTSSGT"] = 35
+  RankOrder["CDTSGT"] = 36
+  RankOrder["CDTCPL"] = 37
+  RankOrder["CDTLCPL"] = 38
+///  RankOrder["CDT"] = 39
+  RankOrder["CDTREC"] = 40
+
+  RankOrder["ADML"] = 1
+  RankOrder["VADM"] = 2
+  RankOrder["RADM"] = 3
+  RankOrder["WO-N"] = 4
+  RankOrder["CDRE"] = 5
+
+  RankOrder["CAPT"] = 6
+  RankOrder["CMDR"] = 7
+  RankOrder["LCDR"] = 8
+  RankOrder["LEUT"] = 9
+  RankOrder["SBLT"] = 10
+  RankOrder["MIDN"] = 12
+
+  RankOrder["WO"] = 16
+  RankOrder["CPO"] = 17
+  RankOrder["PO"] = 19
+  RankOrder["LS"] = 20
+  RankOrder["AB"] = 21
+  RankOrder["SMN"] = 23
+
+  RankOrder["CDTMIDN"] = 29
+///  Could also be (But it's confusing):
+///  RankOrder["MIDN"] = 29
+  RankOrder["CDTWO"] = 33
+  RankOrder["CDTCPO"] = 34
+  RankOrder["CDTPO"] = 36
+  RankOrder["CDTLS"] = 37
+  RankOrder["CDTAB"] = 38
+  RankOrder["CDTSMN"] = 39
+  RankOrder["CDTRCT"] = 40
 }
