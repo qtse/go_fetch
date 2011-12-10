c1b2a69be01ea09381bdd38858a0a0f613762813
Quincy Tse <quincy.tse@sydney.edu.au>
2011-12-10 14:50:54 +1100

    Commenced adding REST support for courses. Added tool to send/receive HTTP messages

--- src/main.go
+++ src/main.go
@@ -3,10 +3,13 @@ package movo
 import (
     "appengine"
     "appengine/delay"
-    "appengine/user"
+///    "appengine/user"
+///    "bytes"
 ///    "fmt"
     "http"
     "os"
+    "strconv"
+    "strings"
     "template"
 ///    "time"
     )
@@ -14,7 +17,54 @@ import (
 func init() {
   http.HandleFunc("/c1Login", c1LoginHandler)
   http.HandleFunc("/c1Logout", c1LogoutHandler)
-  http.HandleFunc("/", root)
+  http.HandleFunc("/index", root)
+  http.HandleFunc("/courses/", courseHandler)
+}
+
+func courseHandler(w http.ResponseWriter, r *http.Request) {
+  c := appengine.NewContext(r)
+
+  path := strings.Split(r.URL.Path, "/")[1:]
+  if path[len(path)-1] == "" {
+    path = path[:len(path)-1]
+  }
+
+  if len(path) == 1 {
+    // All courses
+    switch (r.Method) {
+      case "GET":
+      case "POST":
+        actId,err := strconv.Atoi(r.FormValue("actId"))
+        if err != nil {
+          http.Error(w, "actId not found", http.StatusBadRequest)
+          return
+        }
+
+        skey, err := c1CurrSession(c)
+        if err != nil {
+          http.Error(w, err.String(), http.StatusInternalServerError)
+          return
+        } else if skey == "" {
+          http.Error(w, "C1 Session expired", http.StatusUnauthorized)
+          return
+        }
+        delay.Func("key", fetchActDetail).Call(c, skey, actId)
+        w.WriteHeader(http.StatusAccepted)
+        c.Infof(skey + ": " + strconv.Itoa(actId))
+        w.Write([]byte("ActId:" + strconv.Itoa(actId) + "\n"))
+      case "PUT":
+        fallthrough
+      case "DELETE":
+        w.Header().Add("Allow", "GET")
+        w.Header().Add("Allow", "POST")
+        http.Error(w, "Not Allowed", http.StatusMethodNotAllowed)
+        return
+    }
+  }
+  w.Write([]byte(r.Method + " " + r.URL.String() + " " + strconv.Itoa(len(path)) + " "))
+  for _,s := range path {
+    w.Write([]byte("/" + s))
+  }
 }
 
 type Page struct {
@@ -36,11 +86,18 @@ func root(w http.ResponseWriter, r *http.Request) {
     http.Error(w, err.String(), http.StatusInternalServerError)
     return
   }
+  if l, err := c1IsLoggedIn(c); err != nil {
+    http.Error(w, err.String(), http.StatusInternalServerError)
+    return
+  } else if !l {
+    http.Redirect(w, r, "/c1Login", 302)
+    return
+  }
 
-  var url string
-  var loginText string
-  url,_ = user.LogoutURL(c, "/")
-  loginText = "Logout"
+///  var url string
+///  var loginText string
+///  url,_ = user.LogoutURL(c, "/")
+///  loginText = "Logout"
 
 ///  skey, err := C1Login(c, usr, pwd)
 ///  c.Infof(skey)
@@ -50,10 +107,12 @@ func root(w http.ResponseWriter, r *http.Request) {
 ///  }
 ///  delay.Func("key", fetchActDetail).Call(c, skey, 8525)
 ///  delay.Func("key", fetchNomRoll).Call(c, skey, 8525)
-  delay.Func("key", fetchPerson).Call(c, skey, "8488695")
-  s3 := ""
+///  delay.Func("key", fetchPerson).Call(c, skey, "8488695")
+///  s3 := ""
 
-  l := []string{skey, s3}
-  p := Page{Text: "", TextList: l, Url: url, LoginText: loginText}
-  renderTemplate(w, "template", &p)
+///  l := []string{skey, s3}
+///  p := Page{Text: "", TextList: l, Url: url, LoginText: loginText}
+  p := Page{}
+///  renderTemplate(w, "template", &p)
+  renderTemplate(w, "tool", &p)
 }
